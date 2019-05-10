840e76d8a732d3b568bbd9f3d0001c0522c3ae48
Mauro <mpasserino@irobot.com>
2019-05-10 19:18:29 +0100

    Fix required to switch 3rd party logger to rcl_logging_log4cxx

--- cross-compiling/ignore_pkgs/master_ignore.sh
+++ cross-compiling/ignore_pkgs/master_ignore.sh
@@ -22,6 +22,5 @@ touch \
     src/ros2/ros1_bridge/COLCON_IGNORE \
     src/ros2/rviz/COLCON_IGNORE \
     src/ros2/demos/COLCON_IGNORE \
-    src/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE \
     src/ros/resource_retriever/COLCON_IGNORE \
     src/ros-visualization/COLCON_IGNORE

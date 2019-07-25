ac727f6b512a93079f3221a4b3e5256c1a7258b4
Soragna, Alberto <alberto.soragna@gmail.com>
2019-07-25 12:06:09 +0100

    removed windows system compatibility
    Signed-off-by: Soragna, Alberto <alberto.soragna@gmail.com>

--- performances/performance_test_factory/include/performance_test_factory/load_plugins.hpp
+++ performances/performance_test_factory/include/performance_test_factory/load_plugins.hpp
@@ -15,18 +15,10 @@
 std::string get_env_var(const char * env_var)
 {
   char * value = nullptr;
-#ifndef _WIN32
   value = getenv(env_var);
-#else
-  size_t value_size;
-  _dupenv_s(&value, &value_size, env_var);
-#endif
   std::string value_str = "";
   if (value) {
     value_str = value;
-#ifdef _WIN32
-    free(value);
-#endif
   }
   // printf("get_env_var(%s) = %s\n", env_var, value_str.c_str());
   return value_str;
@@ -40,39 +32,23 @@ std::list<std::string> split(const std::string & value, const char delimiter)
   while (std::getline(ss, s, delimiter)) {
     list.push_back(s);
   }
-  // printf("split(%s) = %zu\n", value.c_str(), list.size());
+
   return list;
 }
 
 bool is_file_exist(const char * filename)
 {
   std::ifstream h(filename);
-  // printf("is_file_exist(%s) = %s\n", filename, h.good() ? "true" : "false");
   return h.good();
 }
 
 std::string find_library_path(const std::string & library_name)
 {
-  const char * env_var;
-  char separator;
-  const char * filename_prefix;
-  const char * filename_extension;
-#ifdef _WIN32
-  env_var = "PATH";
-  separator = ';';
-  filename_prefix = "";
-  filename_extension = ".dll";
-#elif __APPLE__
-  env_var = "DYLD_LIBRARY_PATH";
-  separator = ':';
-  filename_prefix = "lib";
-  filename_extension = ".dylib";
-#else
-  env_var = "LD_LIBRARY_PATH";
-  separator = ':';
-  filename_prefix = "lib";
-  filename_extension = ".so";
-#endif
+  const char * env_var = "LD_LIBRARY_PATH";
+  char separator = ':';
+  const char * filename_prefix = "lib";
+  const char * filename_extension = ".so";
+
   std::string search_path = get_env_var(env_var);
   std::list<std::string> search_paths = split(search_path, separator);
 
@@ -93,17 +69,17 @@ std::string
 get_library_name(std::string msg_type)
 {
   const std::string DEFAULT_LIBRARY_NAME = "irobot_interfaces_plugin";
+  std::string namespace_delimiter = "::";
 
-  std::string library_delimiter = "::";
   std::string library_name;
   //std::string msg_name;
-  size_t pos = msg_type.find(library_delimiter);
+  size_t pos = msg_type.find(namespace_delimiter);
   if (pos == std::string::npos) {
     library_name = DEFAULT_LIBRARY_NAME + "_implementation";
     //msg_name = msg_type;
   } else {
     library_name = msg_type.substr(0, pos) + "_implementation";
-    //msg_name = msg_type.substr(pos + library_delimiter.length(), msg_type.length());
+    //msg_name = msg_type.substr(pos + namespace_delimiter.length(), msg_type.length());
   }
 
   return library_name;

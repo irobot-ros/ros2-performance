151fd985888761e9173142e456cc64c2e2558bac
Mauro Passerino <mpasserino@irobot.com>
2020-02-05 15:52:34 +0000

    Update README and add script to get ROS2 sources

---
+++ cross-compiling/get_ros2_sources.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ROS2_PATH=~/ros2_cc_ws
+
+# You can specify a distribution, otherwise the default one will be used
+distribution="eloquent"
+if [ -z "$2" ]; then
+  echo "Using default ROS2 $distribution distribution sources"
+else
+  distribution=$2
+  echo "Using ROS2 $distribution distribution sources"
+fi
+
+if [ -d $ROS2_PATH ]; then
+    echo "Error: Directory $ROS2_PATH already exists, remove it to get new sources."
+    exit 1
+else
+    # Download sources for distribution
+    echo "Downloading ROS2 sources in directory: $ROS2_PATH"
+    mkdir -p $ROS2_PATH/src
+    cd $ROS2_PATH
+    wget https://raw.githubusercontent.com/ros2/ros2/$distribution/ros2.repos
+    vcs import src < ros2.repos
+fi

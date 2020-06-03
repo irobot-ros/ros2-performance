ab578f2bc1088513fdcf3dc7cb81a0d97fcd64b9
Mauro <mpasserino@irobot.com>
2020-06-03 15:08:34 +0100

    Update automatic_cross_compile.sh

--- get_ros2_sources.sh
+++ get_ros2_sources.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+# You can specify a distribution with --distro=<distribution>, otherwise the default one will be used
+DISTRIBUTION="eloquent"
+
+# You can specify a directory to store sources with --ros2-path=<DIR>, otherwise the default one will be used
 ROS2_PATH=~/ros2_cc_ws
 
-# You can specify a distribution, otherwise the default one will be used
-distribution="eloquent"
-if [ -z "$1" ]; then
-  echo "Using default ROS2 $distribution distribution sources"
-else
-  distribution=$1
-  echo "Using ROS2 $distribution distribution sources"
-fi
+for i in "$@"
+do
+case $i in
+    --distro=*)
+    DISTRIBUTION="${i#*=}"
+    shift
+    ;;
+    --ros2-path=*)
+    ROS2_PATH="${i#*=}"
+    shift
+    ;;
+    -h|--help|*)
+    echo "Usage example: bash get_ros2_sources.sh --distro=master --ros2-path=~/ros2_cc_ws"
+    exit 0
+    shift
+    ;;
+esac
+done
+
+echo "Using ROS2 $DISTRIBUTION distribution sources"
 
 if [ -d $ROS2_PATH ]; then
     echo "Error: Directory $ROS2_PATH already exists, remove it to get new sources."
@@ -19,6 +35,6 @@ else
     echo "Downloading ROS2 sources in directory: $ROS2_PATH"
     mkdir -p $ROS2_PATH/src
     cd $ROS2_PATH
-    wget https://raw.githubusercontent.com/ros2/ros2/$distribution/ros2.repos
+    wget https://raw.githubusercontent.com/ros2/ros2/$DISTRIBUTION/ros2.repos
     vcs import src < ros2.repos
 fi

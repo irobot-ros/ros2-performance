65fb539f4cdf6b8ddd3857a64b7e52ce526fb801
Soragna, Alberto <alberto.soragna@gmail.com>
2020-06-25 06:54:31 +0100

    update automatic cross compile script
    remove sudo calls and return early if steps fail

--- automatic_cross_compile.sh
+++ automatic_cross_compile.sh
@@ -9,8 +9,9 @@ ROS2_DISTRO="${ROS2_DISTRO:=master}"
 # Check that all variables are defined before start
 if [[ -z "$TARGET" ]]
 then
-  echo "Error: environmental variables are not defined!"
-  echo "Set values to TARGET"
+  echo "Error: target architecture is not specified!"
+  echo "Set environment variable TARGET"
+  echo "export TARGET=raspbian"
   exit 1
 fi
 
@@ -20,44 +21,51 @@ BASE_DIR="$(dirname "$(pwd)")"
 # Prepare cross-compiling environment
 source $THIS_DIR/env.sh $TARGET
 
+# Make sure that workspace directory does not exist before running
+WORKSPACE_DIR=$BASE_DIR/ros2_"$ROS2_DISTRO"_"$TARGET_NAME"_ws
+if [ -d "$WORKSPACE_DIR" ]; then
+  echo "Error: workspace directory $WORKSPACE_DIR already exists."
+  echo "Remove it and run again the script."
+  exit 1
+fi
+
+# Get ROS 2 sources
+echo "Create WORKSPACE_DIR=$WORKSPACE_DIR"
+bash $THIS_DIR/get_ros2_sources.sh --distro=$ROS2_DISTRO --ros2-path=$WORKSPACE_DIR
+
+# Ignore packages
+bash $THIS_DIR/ignore_pkgs.sh $WORKSPACE_DIR $ROS2_DISTRO
+
 # Get sysroot
-bash $THIS_DIR/get_sysroot.sh
-
-# Remove ROS2 old cross-compilation workspace and get a new one
-sudo rm -rf $BASE_DIR/ros2_cc_ws
-bash $THIS_DIR/get_ros2_sources.sh --distro=$ROS2_DISTRO --ros2-path=$BASE_DIR/ros2_cc_ws
-
-# Get HEAD of branch
-HEAD=$(git ls-remote git://github.com/ros2/ros2 "$ROS2_DISTRO" | cut -c1-7)
+GET_SYSROOT_CMD="bash $THIS_DIR/get_sysroot.sh"
+if ! $GET_SYSROOT_CMD; then
+  echo "Error: failed to get sysroot for architecture $TARGET"
+  exit 1
+fi
 
 # Create install directory for the cross-compilation results
 TARGET_NAME=${TARGET#*-}
 RESULTS_DIR=$BASE_DIR/ROS2_SDKs/ros2_"$ROS2_DISTRO"_"$TARGET_NAME"
-echo "Create RESULTS_DIR=$RESULTS_DIR"
-sudo rm -rf $RESULTS_DIR
-mkdir -p $RESULTS_DIR
-
-# Save the current packages versions and check if any changes
-ROS2_SRCS_HEADS=$RESULTS_DIR/ros2.repos.by_commit
-ROS2_SRCS_HEADS_PREV_RUN=$BASE_DIR/ros2_srcs_prev_run_"$TARGET_NAME"_"$ROS2_DISTRO".txt
-
-vcs export --exact $BASE_DIR/ros2_cc_ws/src > $ROS2_SRCS_HEADS
-
-if [ -f $ROS2_SRCS_HEADS_PREV_RUN ]; then
-  diff -y  $ROS2_SRCS_HEADS $ROS2_SRCS_HEADS_PREV_RUN > $RESULTS_DIR/diff_ros2_versions.txt
-fi
-cp -R $ROS2_SRCS_HEADS $ROS2_SRCS_HEADS_PREV_RUN
-
-# Ignore packages
-bash $THIS_DIR/ignore_pkgs.sh $BASE_DIR/ros2_cc_ws $ROS2_DISTRO
-
-# Run the cross-compilation and check the return code
-CC_CMD="bash $THIS_DIR/cc_workspace.sh $BASE_DIR/ros2_cc_ws --no-it"
-if $CC_CMD; then
-  # If the build was successful, copy results to store as artifact
-  cp -r $BASE_DIR/ros2_cc_ws/install/* $RESULTS_DIR
-  cd $BASE_DIR/ROS2_SDKs/
-  tar -czf ros2_"$ROS2_DISTRO"_"$TARGET_NAME".tar.gz ros2_"$ROS2_DISTRO"_"$TARGET_NAME"
-else
+if [ -d "$RESULTS_DIR" ]; then
+  echo "Error: results directory $RESULTS_DIR already exists."
+  echo "Remove it and run again the script."
   exit 1
 fi
+echo "Create RESULTS_DIR=$RESULTS_DIR"
+mkdir -p $RESULTS_DIR
+
+# Save the current packages versions
+ROS2_SRCS_HEADS=$RESULTS_DIR/ros2.repos.by_commit
+vcs export --exact $WORKSPACE_DIR/src > $ROS2_SRCS_HEADS
+
+# Run the cross-compilation and check the return code
+CC_CMD="bash $THIS_DIR/cc_workspace.sh $WORKSPACE_DIR --no-it"
+if ! $CC_CMD; then
+  echo "Error: the cross-compilation step failed"
+  exit 1
+fi
+
+# If the build was successful, copy results to store as artifact
+cp -r $WORKSPACE_DIR/install/* $RESULTS_DIR
+cd $BASE_DIR/ROS2_SDKs/
+tar -czf ros2_"$ROS2_DISTRO"_"$TARGET_NAME".tar.gz ros2_"$ROS2_DISTRO"_"$TARGET_NAME"

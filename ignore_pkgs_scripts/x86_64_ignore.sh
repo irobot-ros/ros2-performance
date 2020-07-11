2d68f6f1cbb440ebdcfc8fb53388e0cb99b93a08
Soragna, Alberto <alberto.soragna@gmail.com>
2020-07-11 09:25:29 +0100

    fix ignore pkg script after renaming of kdl library
    see ros repos commit https://github.com/ros2/ros2/commit/69f6a31ffa0b86a6cc169d29b2675c25d96301b2

--- ignore_pkgs_scripts/x86_64_ignore.sh
+++ ignore_pkgs_scripts/x86_64_ignore.sh
@@ -14,14 +14,11 @@ echo "Ignoring packages..."
 # TODO: this may print some errors if some paths are not correct, but it should still work
 # we should ignore paths that do not exist
 touch \
-  src/ros/robot_state_publisher/COLCON_IGNORE \
   src/ros/ros_tutorials/COLCON_IGNORE \
   src/ros2/examples/rclpy/COLCON_IGNORE \
   src/ros2/geometry2/COLCON_IGNORE \
-  src/ros2/kdl_parser/COLCON_IGNORE \
   src/ros2/rclcpp/rclcpp_components/COLCON_IGNORE \
   src/ros2/examples/rclcpp/composition/COLCON_IGNORE \
-  src/ros2/orocos_kinematics_dynamics/COLCON_IGNORE \
   src/ros2/rclpy/COLCON_IGNORE \
   src/ros2/rcl_interfaces/test_msgs/COLCON_IGNORE \
   src/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE \

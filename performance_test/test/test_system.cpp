d6a8a938aadf039d089f27eaeba912ccad0e38ad
Soragna, Alberto <alberto.soragna@gmail.com>
2022-01-22 19:42:41 +0000

    reorganize directories and update to ros2 rolling
    remove intermediate performances and ros2 directories. removed proposals directories. unified READMEs

---
+++ performance_test/test/test_system.cpp
@@ -0,0 +1,130 @@
+/* Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, iRobot ROS
+ *  All rights reserved.
+ *
+ *  This file is part of ros2-performance, which is released under BSD-3-Clause.
+ *  You may use, distribute and modify this code under the BSD-3-Clause license.
+ */
+
+#include <gtest/gtest.h>
+
+#include "performance_test/system.hpp"
+#include "performance_test/node.hpp"
+#include "performance_test_msgs/msg/sample.hpp"
+#include "performance_test_msgs/srv/sample.hpp"
+
+class TestSystem : public ::testing::Test
+{
+public:
+  static void SetUpTestCase()
+  {
+    rclcpp::init(0, nullptr);
+  }
+};
+
+
+TEST_F(TestSystem, SystemAddNodesTest)
+{
+    auto node_1 = std::make_shared<performance_test::Node>("node_1");
+    auto node_2 = std::make_shared<performance_test::Node>("node_2");
+    auto node_3 = std::make_shared<performance_test::Node>("node_3");
+    std::vector<std::shared_ptr<performance_test::Node>> nodes_vec = {node_2, node_3};
+
+    auto system_executor = performance_test::STATIC_SINGLE_THREADED_EXECUTOR;
+    performance_test::System<> separate_threads_system(system_executor);
+
+    separate_threads_system.add_node(node_1);
+    separate_threads_system.add_node(nodes_vec);
+
+    performance_test::System<> single_executor_system(system_executor);
+
+    single_executor_system.add_node(node_1);
+    single_executor_system.add_node(nodes_vec);
+}
+
+
+
+TEST_F(TestSystem, SystemPubSubTest)
+{
+    auto topic = performance_test::Topic<performance_test_msgs::msg::Sample>("my_topic");
+
+    int duration_sec = 1;
+    auto system_executor = performance_test::STATIC_SINGLE_THREADED_EXECUTOR;
+    performance_test::System<> ros2_system(system_executor);
+
+    // Create 1 pulisher node and 1 subscriber node
+    auto pub_node = std::make_shared<performance_test::Node>("pub_node");
+    pub_node->add_periodic_publisher(topic, 10ms, PASS_BY_UNIQUE_PTR, rmw_qos_profile_default);
+    ros2_system.add_node(pub_node);
+
+    auto sub_node = std::make_shared<performance_test::Node>("sub_node");
+    sub_node->add_subscriber(topic, PASS_BY_SHARED_PTR, performance_test::Tracker::TrackingOptions(), rmw_qos_profile_default);
+    ros2_system.add_node(sub_node);
+
+    ros2_system.spin(duration_sec);
+
+    auto trackers_vec_ptr = sub_node->all_trackers();
+    auto tracker = (*trackers_vec_ptr)[0];
+
+    ASSERT_EQ("my_topic", tracker.first);
+    ASSERT_GT(tracker.second.received(), (unsigned long int)1);
+}
+
+
+TEST_F(TestSystem, SystemClientServerTest)
+{
+    auto service = performance_test::Topic<performance_test_msgs::srv::Sample>("my_service");
+
+    int duration_sec = 2;
+    auto system_executor = performance_test::STATIC_SINGLE_THREADED_EXECUTOR;
+    performance_test::System<> ros2_system(system_executor);
+
+    // Create 1 client node and 1 server node
+    auto client_node = std::make_shared<performance_test::Node>("client_node");
+    client_node->add_periodic_client(service, 10ms, rmw_qos_profile_default);
+    ros2_system.add_node(client_node);
+
+    auto server_node = std::make_shared<performance_test::Node>("server_node");
+    server_node->add_server(service, rmw_qos_profile_default);
+    ros2_system.add_node(server_node);
+
+    // discovery check does not work with client/server yet
+    ros2_system.spin(duration_sec, false);
+
+    auto trackers_vec_ptr = client_node->all_trackers();
+    auto tracker = (*trackers_vec_ptr)[0];
+
+    ASSERT_EQ("my_service", tracker.first);
+    ASSERT_GT(tracker.second.received(), (unsigned long int)1);
+}
+
+
+TEST_F(TestSystem, SystemDifferentQoSTest)
+{
+    auto topic = performance_test::Topic<performance_test_msgs::msg::Sample>("my_topic");
+
+    int duration_sec = 1;
+    auto system_executor = performance_test::STATIC_SINGLE_THREADED_EXECUTOR;
+    performance_test::System<> ros2_system(system_executor);
+    rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+
+    // Create 1 pulisher node and 1 subscriber node
+    auto pub_node = std::make_shared<performance_test::Node>("pub_node");
+    qos_profile.reliability = rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+    pub_node->add_periodic_publisher(topic, 10ms, PASS_BY_UNIQUE_PTR, qos_profile);
+    ros2_system.add_node(pub_node);
+
+    auto sub_node = std::make_shared<performance_test::Node>("sub_node");
+    qos_profile.reliability = rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_RELIABLE;
+    sub_node->add_subscriber(topic, PASS_BY_SHARED_PTR, performance_test::Tracker::TrackingOptions(), qos_profile);
+    ros2_system.add_node(sub_node);
+
+    ros2_system.spin(duration_sec);
+
+    auto trackers_vec_ptr = sub_node->all_trackers();
+    auto tracker = (*trackers_vec_ptr)[0];
+
+    // they have incompatible qos so they shouldn't communicate
+    ASSERT_EQ(tracker.second.received(), (unsigned long int)0);
+}
---
+++ performance_test_plugin_cmake/cmake/generate_script.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python3
+
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2019, iRobot ROS
+#  All rights reserved.
+#
+#  This file is part of ros2-performance, which is released under BSD-3-Clause.
+#  You may use, distribute and modify this code under the BSD-3-Clause license.
+#
+
+import argparse
+import os
+import re
+import sys
+
+
+def get_interface_name_from_path(interface_path):
+  '''
+  path has the form msg/PoseWithCovariance.msg
+  the name is only PoseWithCovariance
+  '''
+
+  # get filename from path
+  name_with_extension = os.path.basename(interface_path)
+  # remove extension from filename
+  name = os.path.splitext(name_with_extension)[0]
+  return name
+
+
+def get_lowercased_name(interface_name):
+  '''
+  interface_name has the PoseWithCovariance
+  the lowercased version is pose_with_covariance
+  '''
+
+  # Remove consecutive upper-case letters: HelloWORLD becomes HelloWorld
+  last_upper = False
+  for idx, c in enumerate(interface_name):
+    if c.isupper():
+      if last_upper:
+        interface_name = interface_name[:idx] + c.lower() + interface_name[idx+1:]
+      last_upper = True
+    else:
+      last_upper = False
+
+  # This regular expression splits the name at each capital letter
+  parts = re.sub( r"([A-Z])", r" \1", interface_name).split()
+  lowercase_parts = [x.lower() for x in parts]
+  lowercased_name = "_".join(lowercase_parts)
+  return lowercased_name
+
+
+def get_cpp_include_statement(interface_name, package, interface_type):
+  '''
+  interface_name has the form PoseWithCovariance, in package geometry_msgs and its a "msg"
+  the include statement will be
+  #include "geometry_msgs/msg/pose_with_covariance.hpp"
+  '''
+
+  include_file_name = get_lowercased_name(interface_name) + ".hpp"
+
+  statement = "#include \"" + package + "/" + interface_type + "/" + include_file_name + "\""
+  return statement
+
+
+def get_namespaced_cpp_class_name(interface_name, package, interface_type):
+  '''
+  interface_name has the form PoseWithCovariance, in package geometry_msgs and its a "msg"
+  the namespaced cpp class name will be
+  geometry_msgs::msg::PoseWithCovariance"
+  '''
+
+  class_name = package + "::" + interface_type + "::" + interface_name
+  return class_name
+
+
+def get_include_paths(msgs, srvs, package):
+  '''
+  create include definitions for all the messages and services
+  '''
+
+
+  content = "\n"
+
+  for msg_name in msgs:
+    statement = get_cpp_include_statement(msg_name, package, "msg")
+    content += statement + "\n"
+  content += "\n"
+  for srv_name in srvs:
+    statement = get_cpp_include_statement(srv_name, package, "srv")
+    content += statement + "\n"
+
+  return content
+
+
+def get_sub_factory(msgs, package, node_type):
+
+  if len(msgs) == 0:
+    return ""
+
+  if node_type == "Node":
+    content = """
+
+    extern "C" void add_subscriber_impl(
+    """
+  elif node_type == "LifecycleNode":
+    content = """
+
+    extern "C" void add_subscriber_impl_lifecycle(
+    """
+  else:
+    return ""
+
+  content += "\n std::shared_ptr<performance_test::" + node_type + "> n,"
+
+  content += """
+    std::string msg_type,
+    std::string topic_name,
+    performance_test::Tracker::TrackingOptions tracking_options,
+    msg_pass_by_t msg_pass_by,
+    rmw_qos_profile_t custom_qos_profile)
+  {
+    const std::map<std::string, std::function<void()>> subscribers_factory{
+  """
+
+  function = "add_subscriber"
+  user_args = "msg_pass_by, tracking_options, custom_qos_profile"
+
+  for msg_name in msgs:
+
+    msg_class_name = get_namespaced_cpp_class_name(msg_name, package, "msg")
+    topic = "performance_test::Topic<" + msg_class_name + ">(topic_name)"
+    lowercased_name = get_lowercased_name(msg_name)
+    map_key = "\"" + lowercased_name + "\""
+
+    map_entry = "{" + map_key +",  [&] { n->" + function + "(" + topic +", " + user_args + "); } },"
+
+    content += "\n" + map_entry
+
+  if content.endswith(","):
+    content = content[:-1]
+
+  content += """
+    };"""
+
+  content += """
+
+    if (subscribers_factory.find(msg_type) == subscribers_factory.end()){
+      throw std::runtime_error("unknown msg type passed to subscribers factory: " + msg_type);
+    }
+
+    subscribers_factory.at(msg_type)();
+  }
+
+  """
+
+  return content
+
+
+def get_pub_factory(msgs, package, node_type):
+
+  if len(msgs) == 0:
+    return ""
+
+  if node_type == "Node":
+    content = """
+
+    extern "C" void add_publisher_impl(
+    """
+  elif node_type == "LifecycleNode":
+    content = """
+
+    extern "C" void add_publisher_impl_lifecycle(
+    """
+  else:
+    return ""
+
+  content += "\n std::shared_ptr<performance_test::" + node_type + "> n,"
+
+  content += """
+    std::string msg_type,
+    std::string topic_name,
+    msg_pass_by_t msg_pass_by,
+    rmw_qos_profile_t custom_qos_profile,
+    std::chrono::microseconds period,
+    size_t msg_size)
+  {
+    const std::map<std::string, std::function<void()>> publishers_factory{
+  """
+
+  function = "add_periodic_publisher"
+  user_args = "period, msg_pass_by, custom_qos_profile, msg_size"
+
+  for msg_name in msgs:
+    msg_class_name = get_namespaced_cpp_class_name(msg_name, package, "msg")
+    topic = "performance_test::Topic<" + msg_class_name + ">(topic_name)"
+
+    lowercased_name = get_lowercased_name(msg_name)
+    map_key = "\"" + lowercased_name + "\""
+
+    map_entry = "{" + map_key +",  [&] { n->" + function + "(" + topic +", " + user_args + "); } },"
+
+    content += "\n" + map_entry
+
+  if content.endswith(","):
+    content = content[:-1]
+
+  content += """
+    };"""
+
+  content += """
+
+    if (publishers_factory.find(msg_type) == publishers_factory.end()){
+      throw std::runtime_error("unknown msg type passed to publishers factory: " + msg_type);
+    }
+
+    publishers_factory.at(msg_type)();
+  }
+
+  """
+
+  return content
+
+
+def get_server_factory(srvs, package, node_type):
+
+  if len(srvs) == 0:
+    return ""
+
+  if node_type == "Node":
+    content = """
+
+    extern "C" void add_server_impl(
+    """
+  elif node_type == "LifecycleNode":
+    content = """
+
+    extern "C" void add_server_impl_lifecycle(
+    """
+  else:
+    return ""
+
+  content += "\n std::shared_ptr<performance_test::" + node_type + "> n,"
+
+  content += """
+    std::string srv_type,
+    std::string service_name,
+    rmw_qos_profile_t custom_qos_profile)
+  {
+    const std::map<std::string, std::function<void()>> servers_factory{
+  """
+
+  function = "add_server"
+  user_args = "custom_qos_profile"
+
+  for srv_name in srvs:
+
+    srv_class_name = get_namespaced_cpp_class_name(srv_name, package, "srv")
+    service = "performance_test::Service<" + srv_class_name + ">(service_name)"
+    lowercased_name = get_lowercased_name(srv_name)
+    map_key = "\"" + lowercased_name + "\""
+
+    map_entry = "{" + map_key +",  [&] { n->" + function + "(" + service + ", " + user_args + "); } },"
+
+    content += "\n" + map_entry
+
+  if content.endswith(","):
+    content = content[:-1]
+
+  content += """
+    };"""
+
+  content += """
+
+    if (servers_factory.find(srv_type) == servers_factory.end()){
+      throw std::runtime_error("unknown srv type passed to servers factory: " + srv_type);
+    }
+
+    servers_factory.at(srv_type)();
+  }
+
+  """
+
+  return content
+
+
+def get_client_factory(srvs, package, node_type):
+
+  if len(srvs) == 0:
+    return ""
+
+  if node_type == "Node":
+    content = """
+
+    extern "C" void add_client_impl(
+    """
+  elif node_type == "LifecycleNode":
+    content = """
+
+    extern "C" void add_client_impl_lifecycle(
+    """
+  else:
+    return ""
+
+  content += "\n std::shared_ptr<performance_test::" + node_type + "> n,"
+
+  content += """
+    std::string srv_type,
+    std::string service_name,
+    rmw_qos_profile_t custom_qos_profile,
+    std::chrono::microseconds period)
+  {
+    const std::map<std::string, std::function<void()>> clients_factory{
+  """
+
+  function = "add_periodic_client"
+  user_args = "period, custom_qos_profile"
+
+  for srv_name in srvs:
+
+    srv_class_name = get_namespaced_cpp_class_name(srv_name, package, "srv")
+    service = "performance_test::Service<" + srv_class_name + ">(service_name)"
+    lowercased_name = get_lowercased_name(srv_name)
+    map_key = "\"" + lowercased_name + "\""
+
+    map_entry = "{" + map_key +",  [&] { n->" + function + "(" + service +", " + user_args + "); } },"
+
+    content += "\n" + map_entry
+
+  if content.endswith(","):
+    content = content[:-1]
+
+  content += """
+    };"""
+
+  content += """
+    if (clients_factory.find(srv_type) == clients_factory.end()){
+      throw std::runtime_error("unknown srv type passed to clients factory: " + srv_type);
+    }
+
+    clients_factory.at(srv_type)();
+ }
+
+  """
+
+  return content
+
+
+def main():
+
+  parser = argparse.ArgumentParser(description='Python script for generating interfaces implementation')
+  parser.add_argument('output_path', type=str)
+  parser.add_argument('--package', type=str)
+  parser.add_argument('--msg', type=str, nargs='+', default=[])
+  parser.add_argument('--srv', type=str, nargs='+', default=[])
+
+  args = parser.parse_args()
+
+  output_file_path = args.output_path
+  package = args.package
+  msg_paths = args.msg
+  srv_paths = args.srv
+
+  if not msg_paths and not srv_paths:
+    sys.exit('No interfaces!')
+
+  msgs = []
+  srvs = []
+
+  for path in msg_paths:
+    msgs.append(get_interface_name_from_path(path))
+  for path in srv_paths:
+    srvs.append(get_interface_name_from_path(path))
+
+  outdir = os.path.dirname(output_file_path)
+  os.makedirs(outdir, exist_ok=True)
+
+  content = """
+  #include "performance_test/node.hpp"
+  #include "performance_test/lifecycle_node.hpp"
+
+  """
+
+  content += get_include_paths(msgs, srvs, package)
+  content += get_sub_factory(msgs, package, "Node")
+  content += get_sub_factory(msgs, package, "LifecycleNode")
+
+  content += get_pub_factory(msgs, package, "Node")
+  content += get_pub_factory(msgs, package, "LifecycleNode")
+
+  content += get_server_factory(srvs, package, "Node")
+  content += get_server_factory(srvs, package, "LifecycleNode")
+
+  content += get_client_factory(srvs, package, "Node")
+  content += get_client_factory(srvs, package, "LifecycleNode")
+
+  def create(filename, content):
+    if os.path.exists(filename):
+      old_content = open(filename, 'r').read()
+      if old_content == content:
+        return
+
+    open(filename, 'w').write(content)
+
+  create(output_file_path, content)
+
+
+
+if __name__ == "__main__":
+  main()

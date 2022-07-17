ba076b358312a00de587070bf14c2645cefff8f8
Alberto Soragna <alberto.soragna@gmail.com>
2022-07-17 22:09:17 +0100

    fix cyclone path and update ci
    Signed-off-by: Alberto Soragna <alberto.soragna@gmail.com>

--- scripts/rmw/rmw_cyclonedds_cpp/setup.sh
+++ scripts/rmw/rmw_cyclonedds_cpp/setup.sh
@@ -3,7 +3,7 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-export CYCLONEDDS_URI=file://$PWD/zero-copy-shm.xml
+export CYCLONEDDS_URI=file://$THIS_DIR/zero-copy-shm.xml
 iox-roudi &
 
 echo "Enablig shared memory zero-copy transport for cyclonedds"
